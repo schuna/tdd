ec81b9a8126c972f9aa3350e9b846083cf9aeefe
woorunwoo <jiyong1972@gmail.com>
2021-12-05 14:47:14 +0900

    ParameterizedTest를 이용한 테스트 리펙토링

--- src/test/PasswordStrengthMeterTest.java
+++ src/test/PasswordStrengthMeterTest.java
@@ -1,44 +1,28 @@
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import passwordmeter.PasswordStrength;
 import passwordmeter.PasswordStrengthMeter;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PasswordStrengthMeterTest {
 
-    @DisplayName("세가지 조건을 만족하는 경우 1")
-    @Test
-    void meetAllCriteriaThenStrongTest1() {
-        assertPasswordStrengthEquals("fgtd1@AB", PasswordStrength.STRONG);
+    @DisplayName("세가지 조건을 모두 만족하는 경우")
+    @ParameterizedTest(name = "[{index}] password = {0}")
+    @ValueSource(strings = {"fgtd1@AB", "ab2D1!fg"})
+    void meetAllCriteriaThenStrongTest(String password) {
+        assertPasswordStrengthEquals(password, PasswordStrength.STRONG);
     }
 
-    @DisplayName("세가지 조건을 만족하는 경우 2")
-    @Test
-    void meetAllCriteriaThenStrongTest2() {
-        assertPasswordStrengthEquals("ab2D1!fg", PasswordStrength.STRONG);
+    @DisplayName("두가지 조건을 모두 만족하는 경우")
+    @ParameterizedTest(name = "[{index}] password = {0}")
+    @ValueSource(strings = {"ab2d1!fg", "G!drh@jr", "A34g4&3"})
+    void meetTwoCriteriaThenNormalTest(String password) {
+        assertPasswordStrengthEquals(password, PasswordStrength.NORMAL);
     }
 
-    @DisplayName("길이와 숫자, 두가지 조건을 만족하는 경우")
-    @Test
-    void meetLengthAndNumberCriteriaThenNormalTest() {
-        assertPasswordStrengthEquals("ab2d1!fg", PasswordStrength.NORMAL);
-    }
-
-    @DisplayName("길이와 대문자, 두가지 조건을 만족하는 경우")
-    @Test
-    void meetLengthAndUpperCaseCriteriaThenNormalTest() {
-        assertPasswordStrengthEquals("G!drh@jr", PasswordStrength.NORMAL);
-    }
-
-    @DisplayName("숫자와 대문자, 두가지 조건을 만족하는 경우")
-    @Test
-    void meetNumberAndUpperCaseCriteriaThenNormalTest() {
-        assertPasswordStrengthEquals("A34g4&3", PasswordStrength.NORMAL);
-    }
-
-    private void assertPasswordStrengthEquals(String password, PasswordStrength expected){
+    private void assertPasswordStrengthEquals(String password, PasswordStrength expected) {
         PasswordStrengthMeter passwordstrengthmeter = new PasswordStrengthMeter();
         PasswordStrength result = passwordstrengthmeter.measurePasswordStrength(password);
         assertEquals(expected, result);
