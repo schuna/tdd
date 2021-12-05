e1ae17ac8a56e79183d60a9cf62de2d2c9722598
woorunwoo <jiyong1972@gmail.com>
2021-12-05 15:22:42 +0900

    Parameterized Test 추가

--- src/test/CalculatorTest.java
+++ src/test/CalculatorTest.java
@@ -1,16 +1,18 @@
 import calculator.Calculator;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
     @DisplayName("정수 다하기 정상 케이스")
-    @Test
-    void testAdd2IntegerNormal() {
+    @ParameterizedTest(name = "[{index}] {0} + {1} = {2}")
+    @CsvSource({"1, 2, 3", "4, 5, 9"})
+    void testAdd2IntegerNormal(int a, int b, int expectedSum) {
         Calculator cal;
         cal = new Calculator();
-        int result = cal.plus(1, 2);
-        assertEquals(3, result);
+        int result = cal.plus(a, b);
+        assertEquals(expectedSum, result);
     }
 }
